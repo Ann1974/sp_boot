d2914c14f36d6fe4e0fe562867d1722bc1705edc
Anna Kuryleva <moreematriks@gmail.com>
2023-04-20 11:37:55 +0300

    update all class

--- src/main/java/anna/kata/sp_boot/dao/UserDaoImpl.java
+++ src/main/java/anna/kata/sp_boot/dao/UserDaoImpl.java
@@ -1,33 +1,47 @@
 package anna.kata.sp_boot.dao;
 
 import anna.kata.sp_boot.model.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 @Repository
 public class UserDaoImpl implements UserDao{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public void saveUser(User user) {
+        entityManager.persist(user);
+    }
 
+
+    @Override
+    public void removeUserById(Long id) {
+        User user = entityManager.find(User.class, id);
+        if (user != null) {entityManager.remove(user);
+        }
     }
 
     @Override
     public void updateUser(User updateUser) {
-
+        entityManager.merge(updateUser);
     }
 
-    @Override
-    public void removeUserById(Long id) {
 
-    }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        return entityManager.createQuery("SELECT user from User user", User.class).getResultList();
     }
 
     @Override
     public User getUserById(Long id) {
-        return null;
+        return entityManager.find(User.class, id);
     }
 }
+
+
